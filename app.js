2efb3d4eb44f8e09e61f7f116efb76654db07fc4
Abid <ghulamabidhassan@gmail.com>
2022-12-03 23:40:19 +0400

    Js click events done

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ import jsonFile from "./data.json" assert { type: "json" };
 
 const rightContainer = document.querySelector(".right");
 
+const commonData = jsonFile.map((item) => {
+  return item;
+});
+
 const dataMap = jsonFile
   .map((items) => {
     const { title, background, image } = items;
@@ -17,8 +21,8 @@ const dataMap = jsonFile
       <img src="./images/icon-ellipsis.svg" alt="" class="dots" />
     </div>
     <div class="div-2">
-      <h3 class="hours">${weekly.current}hrs</h3>
-      <p class="last-week">last week - ${weekly.previous}hrs</p>
+      <h3 class="hours"> <span class="daily-hours">${weekly.previous}</span>hrs </h3>
+      <p class="last-week">last week - <span class="week-hours">${weekly.previous}</span>hrs</p>
     </div>
   </article>
 </section>`;
@@ -26,3 +30,67 @@ const dataMap = jsonFile
   .join("");
 
 rightContainer.innerHTML = dataMap;
+
+const days = document.querySelectorAll(".days");
+const daily = document.querySelector(".daily");
+const weekly = document.querySelector(".weekly");
+const monthly = document.querySelector(".monthly");
+const hours = document.querySelectorAll(".daily-hours");
+const weekHours = document.querySelectorAll(".week-hours");
+
+days.forEach((day) => {
+  day.addEventListener("click", function () {
+    days.forEach((element) => {
+      element.classList.remove("active-white");
+      this.classList.add("active-white");
+    });
+  });
+});
+
+daily.addEventListener("click", () => {
+  let data = commonData;
+  let currentData = [];
+  let dailyData = [];
+
+  for (let i = 0; i < data.length; i++) {
+    currentData.push(data[i].timeframes.daily);
+    dailyData = [...currentData];
+
+    for (let h = 0; h < dailyData.length; h++) {
+      hours[h].innerText = dailyData[h].current;
+      weekHours[h].innerText = dailyData[h].previous;
+    }
+  }
+});
+
+weekly.addEventListener("click", () => {
+  let data = commonData;
+  let currentData = [];
+  let dailyData = [];
+
+  for (let i = 0; i < data.length; i++) {
+    currentData.push(data[i].timeframes.weekly);
+    dailyData = [...currentData];
+
+    for (let h = 0; h < dailyData.length; h++) {
+      hours[h].innerText = dailyData[h].current;
+      weekHours[h].innerText = dailyData[h].previous;
+    }
+  }
+});
+
+monthly.addEventListener("click", () => {
+  let data = commonData;
+  let currentData = [];
+  let dailyData = [];
+
+  for (let i = 0; i < data.length; i++) {
+    currentData.push(data[i].timeframes.monthly);
+    dailyData = [...currentData];
+
+    for (let h = 0; h < dailyData.length; h++) {
+      hours[h].innerText = dailyData[h].current;
+      weekHours[h].innerText = dailyData[h].previous;
+    }
+  }
+});
